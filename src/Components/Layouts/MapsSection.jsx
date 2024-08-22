ede1846cf0b724de9def5223da968c8abe56dcc7
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-08-22 21:20:55 +0700

    feat: adding maps component

---
+++ src/Components/Layouts/MapsSection.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const MapsSection = () => {
+  return (
+    <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.331569369317!2d112.61192199999999!3d-7.964645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7882871a4c16c3%3A0xbf6a7513d4d4976!2sJl.%20Raya%20Candi%20II%20No.75%2C%20Karangbesuki%2C%20Kec.%20Sukun%2C%20Kota%20Malang%2C%20Jawa%20Timur%2065146!5e0!3m2!1sen!2sid!4v1724335823808!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+    </div>
+  )
+}
+
+export default MapsSection
